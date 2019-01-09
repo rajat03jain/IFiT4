85e547d68d57445061878e852f3c0a2e2222d28d
Rajatjain47 <rajat06jain47@gmail.com>
2019-01-09 23:02:05 +0530

    adding more

---
+++ app/src/main/java/com/example/hp/ifit/GetBedAdapter.java
@@ -0,0 +1,78 @@
+package com.example.hp.ifit;
+
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class GetBedAdapter extends RecyclerView.Adapter<GetBedAdapter.ViewHolder> {
+
+    private ArrayList<GetBedDao> beddao;
+
+    public GetBedAdapter(ArrayList<GetBedDao> getBedDao){
+        this.beddao= getBedDao;
+    }
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.hospital_list_binder, viewGroup, false);
+        final ViewHolder mViewHolder = new ViewHolder(view);
+        return mViewHolder;
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+
+        viewHolder.hospname.setText(beddao.get(i).getHospname());
+        viewHolder.hospimage.setImageResource(beddao.get(i).getHospimage());
+        viewHolder.address.setText(beddao.get(i).getAddress());
+        viewHolder.rrr.setText(beddao.get(i).getRrr());
+       /* viewHolder.cat.setText(beddao.get(i).getAbout());
+        viewHolder.bedimage.setImageResource(beddao.get(i).getImage());
+        viewHolder.price.setText(String.valueOf(beddao.get(i).getPrice()));*/
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return beddao.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView hospname,address,rrr;
+        ImageView hospimage;
+        Button callambulance, getroom;
+/*
+        TextView id;
+        TextView price;
+        TextView cat;
+        ImageView bedimage;*/
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            hospname= itemView.findViewById(R.id.hospname);
+            address= itemView.findViewById(R.id.hospaddress);
+            rrr= itemView.findViewById(R.id.rrrtext);
+            hospimage= itemView.findViewById(R.id.hospital_image);
+            callambulance= itemView.findViewById(R.id.bookambulancebutton);
+            getroom= itemView.findViewById(R.id.bookroombutton);
+            /*price= itemView.findViewById(R.id.bedprice);
+            cat= itemView.findViewById(R.id.bedcat);
+            bedimage= itemView.findViewById(R.id.bedimage);*/
+
+
+        }
+    }
+}
+
---
+++ app/src/main/java/com/example/hp/ifit/HospitalBedData.java
@@ -0,0 +1,9 @@
+package com.example.hp.ifit;
+
+public class HospitalBedData {
+    static String [] hname={"AIIMS", "FORTIS","BL KAPOOR", "BhimRao"};
+    static Integer[] himage={R.drawable.aims, R.drawable.fortis, R.drawable.blk, R.drawable.bhimrao};
+    static String [] haddress={"Ansari Nagar, Aurobindo Marg","S-549, Near Don Bosco School, Greater Kailash – II","Pusa Road","Bhagawan Mahavir Marg, Sector-6, Rohini"};
+    static String[] rr={"?","???","??","???"};
+
+}
