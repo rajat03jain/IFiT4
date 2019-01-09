ea4a136143b73aaa0829f7fb1557d5e1e0893b2d
Rajatjain47 <rajat06jain47@gmail.com>
2019-01-09 22:57:21 +0530

    adding more

--- app/src/main/java/com/example/hp/ifit/GetBed.java
+++ app/src/main/java/com/example/hp/ifit/GetBed.java
@@ -2,12 +2,43 @@ package com.example.hp.ifit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
 
 public class GetBed extends AppCompatActivity {
+    private ArrayList<GetBedDao> beddaolist = new ArrayList<GetBedDao>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_bed);
+        initGetBedRecycler();
+    }
+
+
+
+    private void initGetBedRecycler() {
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        RecyclerView recyclerView = findViewById(R.id.getbed_recycler);
+        recyclerView.setLayoutManager(layoutManager);
+        beddaolist = new ArrayList<GetBedDao>();
+        for (int i = 0; i < HospitalBedData.hname.length-1; i++) {
+            beddaolist.add(new GetBedDao(
+                    HospitalBedData.hname[i],
+                    HospitalBedData.haddress[i],
+                    HospitalBedData.rr[i],
+                    HospitalBedData.himage[i]
+            ));
+        }
+
+
+
+        GetBedAdapter adapter = new GetBedAdapter( beddaolist);
+        recyclerView.setAdapter(adapter);
     }
 }
+
